e99ad86b006b4c9d5327172eedcf3cb96feb5f26
Juan Velez Ballesteros <juanvelezballesteros@gmail.com>
2016-11-25 11:53:37 -0600

    soluciion de requests

--- proyectofinal/Bancos/grails-app/conf/spring/resources.groovy
+++ proyectofinal/Bancos/grails-app/conf/spring/resources.groovy
@@ -1,11 +1,4 @@
-import grails3.cors.interceptor.SpringSecurityCorsFilter
-import org.springframework.boot.context.embedded.FilterRegistrationBean
-import grails3.cors.interceptor.SpringSecurityCorsFilter
 
 beans = {
-	securityCorsFilter(SpringSecurityCorsFilter)
-	 myFilterDeregistrationBean(FilterRegistrationBean) {
-        filter = ref('securityCorsFilter')
-        enabled = false
-    }
+	corsFilter(CorsFilter)
 }
